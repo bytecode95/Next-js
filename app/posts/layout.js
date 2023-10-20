12d3d910a393645e8a1ac20de7021dc9138dbce6
chethanavirajini <cheth.virajini@gmail.com>
2023-10-20 23:10:14 +0530

    loading

---
+++ app/posts/layout.js
@@ -0,0 +1,12 @@
+//this layout is used to share the UI components between routes in post 
+
+
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>Naviagte to Top</div>
+  )
+}
+
+export default layout
---
+++ app/posts/loading.js
@@ -0,0 +1,4 @@
+const Loading = () =>{
+   //you can add any UI inside loading including skelton
+   return;
+}
