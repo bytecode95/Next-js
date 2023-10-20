a9c8a54a6e7fd571495a3c6d412d129953da6208
chethanavirajini <cheth.virajini@gmail.com>
2023-10-20 23:25:38 +0530

    error handling

---
+++ app/posts/error.js
@@ -0,0 +1,17 @@
+'use client' //Error mush be client components
+
+import React, { useEffect } from 'react'
+
+const error = () => {
+
+    useEffect=()=>{
+
+    }
+
+    
+  return (
+    <div>error</div>
+  )
+}
+
+export default error
